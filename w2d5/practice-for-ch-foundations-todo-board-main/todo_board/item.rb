e503f77fef617dc61ffbe090f839795357d40136
Jet Li <jetpoke1011@gmail.com>
2023-05-15 02:32:15 -0400

    Update item.rb

--- w2d5/practice-for-ch-foundations-todo-board-main/todo_board/item.rb
+++ w2d5/practice-for-ch-foundations-todo-board-main/todo_board/item.rb
@@ -2,8 +2,48 @@
 
 class Item 
 
-    def valid_date?(data_string)
+    def self.valid_date?(data_string)
     end 
 
 
-    def 
+    def initialize(title, deadline, description)
+        @title = title
+        @deadline = deadline
+        @description = description
+    end
+
+    def title 
+        @title
+    end
+
+    def title=(new_title)
+        @title = new_title
+    end 
+
+
+    def deadline 
+        @deadline
+    end 
+
+
+    def deadline=(n_deadline)
+        if Item.valid_date?(n_deadling)
+            @deadline = n_deadline
+        end 
+
+    end 
+
+
+    def description
+        @description
+    end 
+
+
+
+    def description=(n_description)
+        @description = n_description
+    end 
+end 
+
+
+
